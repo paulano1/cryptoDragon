a620a731ebcca7f48341ca63bdbb5e2e771b8c5f
Anomitro Paul <anomitropaul@anomitros-mbp.myfiosgateway.com>
2022-08-24 09:33:41 -0400

    env vars

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 
 intents = discord.Intents.default()
 intents.message_content = True
-
+token = os.getenv('token')
 client = discord.Client(intents=intents)
 
 @client.event
@@ -19,5 +19,6 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
-client.run('MTAxMTY1NzQ3MTgyMTA5OTE2OA.GhRUuU.fGroURMPPajT8T-dUMPowJ6FNe2wQi_VfbEq24')
+#client.run('MTAxMTY1NzQ3MTgyMTA5OTE2OA.GhRUuU.fGroURMPPajT8T-dUMPowJ6FNe2wQi_VfbEq24')
 #client.run('MTAxMTY1NzQ3MTgyMTA5OTE2OA.GSd7km.96bO2sbo3Zs8VLWW5RNqDXFhMliC5lpcQZbpAk')
+client.run(token)
