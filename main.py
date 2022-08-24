b34b3f03c0a7e2328d0552a69935cb5fd3886e73
Anomitro Paul <anomitropaul@anomitros-mbp.myfiosgateway.com>
2022-08-24 10:58:46 -0400

    timing feature.1.2

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ async def on_ready():
 
 @client.event
 async def on_ready():
-    #if dt_hr == 10:
-    print("bot:user ready == {0.user}".format(client))
-    channel = client.get_channel(newsChannelID)
-    await channel.send(f"checking news channel {dt_hr}")
+    if dt_hr == 14:
+        print("bot:user ready == {0.user}".format(client))
+        channel = client.get_channel(newsChannelID)
+        await channel.send(f"checking news channel {dt_hr}")
 
 
 @client.event
